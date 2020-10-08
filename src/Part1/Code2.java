165372dce6576ad01359549bcddcf6368c6a7b7b
Vivek-610 <610vivek@gmail.com>
2020-10-09 00:49:19 +0530

    tring change

--- src/Part1/Code2.java
+++ src/Part1/Code2.java
@@ -3,6 +3,21 @@ package Part1;
 import java.util.Scanner;
 
 public class Code2{
+	public static void main(String args[]){
+
+		String Name;
+		System.out.println("\n\tPractical-2\n");
+		Scanner sc= new Scanner(System.in);
+
+		System.out.println("\nEnter the String:- ");
+		Name = sc.nextLine();
+		System.out.println("Length of String\t"+Name.length());
+
+		endEs(Name);
+		System.out.println("\n19CE055 Vivek D Kapadia\n");
+		sc.close();
+	}
+
 	static void endEs(String temp){
 		if(temp.endsWith("es")){
 			System.out.println("\nEnds with:- es \n");
@@ -16,21 +31,6 @@ public class Code2{
 		else{
 			System.out.println("\nNo es found\n");
 		}
-	}
-	
-	public static void main(String args[]){
-		
-		String Name;
-		System.out.println("\n\tPractical-2\n");
-		Scanner sc= new Scanner(System.in);
-		
-		System.out.println("\nEnter the String:- ");
-		Name = sc.nextLine();
-		System.out.println("Length of String\t"+Name.length());
-		
-		endEs(Name);
-		System.out.println("\n19CE055 Vivek D Kapadia\n");
-		sc.close();
 	}
 }	
 		
